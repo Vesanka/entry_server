15ebdf32a3c69c8d92719cde5ed81cc463fc8199
Vezanka <aleks_shapiro@mail.ru>
2021-09-06 15:44:40 +0300

    added logging

--- server/main.py
+++ server/main.py
@@ -3,14 +3,20 @@ from http.server import (
     BaseHTTPRequestHandler,
 )
 import json
+import sqlite3
 
 from sqlite_classes import (
     SqliteManager,
 )
+from methods import (
+    LogManager,
+)
+
+logger = LogManager('server/log.json')
 
 sql_manager = SqliteManager('test.db')
 
-sql_table_name = 'testtable'
+SQL_TABLE_NAME = 'testtable'
 
 redislist = {}
 
@@ -19,42 +25,69 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     global sql_manager
 
-    def do_GET(self):
-
-        self.send_response(200)
+    def send_resp(self, response):
+        self.send_response(response)
         self.send_header('content-type', 'text/html')
         self.end_headers()
+
+    def do_GET(self):
+
         output = ''
 
         if self.path.endswith('get_sqlite'):
-            for value in sql_manager.do_select_all(sql_table_name):
+            self.send_resp(200)
+            for value in sql_manager.do_select_all(SQL_TABLE_NAME):
                 output += str(value)
             self.wfile.write(output.encode())
 
-        if self.path.endswith('get_redis'):
+        elif self.path.endswith('get_redis'):
+            self.send_resp(200)
             for key, value in redislist.items():
                 output += f'{key}: {value}\n'
             self.wfile.write(output.encode())
 
+        elif self.path.endswith('get_logs'):
+            self.send_resp(200)
+            self.wfile.write(logger.get_log().encode())
+
+        else:
+            self.send_resp(404)
+
     def do_POST(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
 
         data_string = self.rfile.read(int(self.headers['Content-Length']))
-        data = json.loads(data_string)
 
         if self.path.endswith('post_sqlite'):
-            sql_manager.do_insert(sql_table_name, data_string)
+            try:
+                data = json.loads(data_string)
+                sql_manager.do_insert(SQL_TABLE_NAME, data_string)
+            except (json.JSONDecodeError, sqlite3.OperationalError) as err:
+                self.send_resp(400)
+                logger.error(code='400', headers=self.headers, error=err.__class__)
+            else:
+                self.send_resp(200)
+                logger.info(code='200', headers=self.headers, body=data_string)
 
+        elif self.path.endswith('post_redis'):
+            try:
+                data = json.loads(data_string)
+            except json.JSONDecodeError:
+                self.send_resp(400)
+            else:
+                self.send_resp(200)
+                redislist.update(data)
 
-        if self.path.endswith('post_redis'):
-            redislist.update(data)
+        else:
+            self.send_resp(404)
 
 
 def main():
-    
-    sql_manager.do_create(table_name=sql_table_name, name='TEXT', number='INTEGER')
+
+    sql_manager.do_create(
+        table_name=SQL_TABLE_NAME,
+        name='TEXT', 
+        number='INTEGER',
+    )
 
     PORT = 9000
     server_addres = ('localhost', PORT)
---
+++ server/methods.py
@@ -0,0 +1,33 @@
+import json
+import datetime
+
+class LogManager(object):
+
+    def __init__(self, logfile_name):
+        self.logfile_name = logfile_name
+    
+    def info(self, code, headers, body):
+        with open(self.logfile_name, 'a+') as file:
+            data = {
+                'timestamp': datetime.datetime.now().timestamp(),
+                'code': str(code),
+                'headers' : str(headers),
+                'body': str(body),
+            }
+            json.dump(data, file)
+            file.write('\n')
+    
+    def error(self, code, headers, error):
+        with open(self.logfile_name, 'a+') as file:
+            data = {
+                'timestamp': datetime.datetime.now().timestamp(),
+                'code': str(code),
+                'headers' : str(headers),
+                'error': str(error),
+            }
+            json.dump(data, file)
+            file.write('\n')
+
+    def get_log(self):
+        with open(self.logfile_name, 'r+') as file:
+            return file.read()
